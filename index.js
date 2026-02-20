2a1068c787db3919b731a4ab43779a3691343f8d
kelvynkohler95-ponto-com <kadreykohler@gmail.com>
2026-02-20 19:59:57 -0300

    Desafio do heroi

---
+++ index.js
@@ -0,0 +1,21 @@
+const herois = [
+    { nome: "Enzo", xp: 1500 },
+    { nome: "Arthur", xp: 8500 },
+    { nome: "LogicaMan", xp: 10500 }
+];
+
+for (let i = 0; i < herois.length; i++) {
+    let heroi = herois[i];
+    let nivel = "";
+
+    if (heroi.xp < 1000) nivel = "Ferro";
+    else if (heroi.xp <= 2000) nivel = "Bronze";
+    else if (heroi.xp <= 5000) nivel = "Prata";
+    else if (heroi.xp <= 7000) nivel = "Ouro";
+    else if (heroi.xp <= 8000) nivel = "Platina";
+    else if (heroi.xp <= 9000) nivel = "Ascendente";
+    else if (heroi.xp <= 10000) nivel = "Imortal";
+    else nivel = "Radiante";
+
+    console.log(`O Herói de nome **${heroi.nome}** está no nível de **${nivel}**`);
+}
